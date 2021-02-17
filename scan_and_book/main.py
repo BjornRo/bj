0b1adbfa6cc205ffd506ef6275510e2ac8e8e9ca
BjornRo <54636059+BjornRo@users.noreply.github.com>
2021-02-17 12:18:41 +0100

    Implemented all required functions

--- scan_and_book/main.py
+++ scan_and_book/main.py
@@ -5,13 +5,8 @@ import time
 import json
 from datetime import datetime, timedelta
 
-# time_slot: "" or "19:45-21:45"...
-time_slot = ""
-where = "Centrum"  # Sisjön, Partille
-
 # Search every # seconds.
 search_frequency = 20
-
 
 # Load JSON data
 with open("data.json", "r") as f:
@@ -24,6 +19,9 @@ sub_url = data["site"]["sub"]
 username = data["login"]["username"]
 password = data["login"]["password"]
 
+
+#https://fysiken.nu/sv/boka?units=1133&units=1742&units=1766&locale=sv-SE
+
 # Functions
 def sort_and_order_bookinglist(soup: list):
     # Find the bookings
@@ -34,16 +32,25 @@ def sort_and_order_bookinglist(soup: list):
     # Empty dict
     booking_list = {}
 
+    # Time related, to sort out unavailable times.
+    tf = "%H:%M"
+    tb = "[0-9]+:[0-9]+"
+    time_now = datetime.strptime(datetime.now().strftime(tf), tf)
+
     # Add all relevant data for each booking at each location
     for i in souplist:
         # Main key
         location = re.sub("\n|\r|\(|\)", "", i.find("div", class_="location").text.strip())
-        time_activity = re.sub(" |\n|\r", "", i.find("div", class_="time").text)
-        booking_url = site + i.find("a")["href"]
+        time_book = re.sub(" |\n|\r", "", i.find("div", class_="time").text)
+        booking_url = site + i.find("div", class_="button-holder").find("a")["href"]
         slots = re.search(":(>[0-9]+|[0-9]+)", re.sub(" |\n|\r", "", i.find("div", class_="status").text))
 
-        # If slots is empty, then just ignore(continue the loop)
+        # If slots is empty(drop in etc), then just ignore(continue the loop)
         if not slots:
+            continue
+
+        # Check if all slots are taken and there is 2hours or less, then continue. You can't unbook less than 2hours.
+        if slots[1] == "0" and datetime.strptime(re.search(tb, time_book)[0], tf) - time_now <= timedelta(hours=2):
             continue
 
         # If current location doesn't exist, add an empty dict
@@ -51,28 +58,44 @@ def sort_and_order_bookinglist(soup: list):
             booking_list[location] = {}
 
         # Add time as key, then bookingurl and slots in a tuple
-        booking_list[location][time_activity] = (booking_url, slots.group(1))
+        booking_list[location][time_book] = (booking_url, slots[1])
     return booking_list
 
 
-def select_booking_input(bookingslist: list):
-    # If there are no more times, return current time.
-    if not bookingslist:
-        return datetime.now().strftime("%H:%M")
-    print("Select your time:")
-    for i, elem in enumerate(bookingslist):
-        btime = re.sub(" |\n|\r", "", elem.find("div", class_="time").text)
-        bslots = re.search(":([0-9]+|>[0-9]+)", re.sub(" |\n|\r", "", elem.find("div", class_="status").text))
-        print(f"  {i}: {btime}, slots: {bslots.group(1)}")
+def select_location(bookingslist: list):
+    # Key for location
+    loc_keys = list(bookingslist)
+
+    # Print all locations
+    print("Select location:")
+    for i, elem in enumerate(loc_keys):
+        print(f"  {i}: {elem}")
+
+    # Get user input and check if it's valid
     user_in = input()
     if user_in.isdigit() and (0 <= int(user_in) < len(bookingslist)):
-        return re.sub(" |\n|\r", "", bookingslist[int(user_in)].find("div", class_="time").text)
+        return bookingslist[loc_keys[int(user_in)]]
     print("Enter a valid input!")
-    print_times_and_get_user_input(bookingslist)
+    select_location(bookingslist)
+
+
+def select_time(bookingslist: list):
+    # Key for time
+    time_keys = list(bookingslist)
+
+    print("Select your time:")
+    for i, elem in enumerate(time_keys):
+        print(f"  {i}: {time_keys[i]}, slots: {bookingslist[elem][1]}")
+    user_in = input()
+    if user_in.isdigit() and (0 <= int(user_in) < len(bookingslist)):
+        return bookingslist[time_keys[int(user_in)]][0]
+    print("Enter a valid input!")
+    select_time(bookingslist)
 
 
 def main():
-    global time_slot
+    # Set to None, to make the algorithm try to automatically book the later selected time.
+    timeslot_link = None
 
     # Flag if it hasn't booked.
     booked = False
@@ -96,59 +119,20 @@ def main():
                 print("No available times for the day")
                 break
 
-            # If there is no specified time_slot, print and let user choose.
-            # Also check if the timeslot is valid
-            if not time_slot:
-                time_slot = select_booking_input(all_bookings)
-            else:
-                for i in bookings:
-                    if time_slot in i.text.replace(" ", ""):
-                        break
-                else:
-                    print("Invalid time specified")
-                    break
+            # Select Booking
+            if not timeslot_link:
+                timeslot_link = select_time(select_location(all_bookings))
 
-            # Check if there is more than two hours to the time chosen.
-            if datetime.strptime(re.search("[0-9]+:[0-9]+", time_slot).group(0), "%H:%M") - datetime.strptime(
-                datetime.now().strftime("%H:%M"), "%H:%M"
-            ) <= timedelta(hours=2):
-                print("No available times for the day, less than two hours")
-                break
-
-            # Next step get the timeslot link.
-            timeslot_url = timeslot_link(bookings)
-
-            if timeslot_url:
-                # TODO
-                booked = True  # post_data(timeslot_url)
+            booked = True  # post_data(timeslot_link)
 
         if not booked:
             print("No slots")
             time.sleep(search_frequency)
 
 
-# Gets the link for the timeslot
-# Else None if not found/full.
-def timeslot_link(all_time_slots: list):
-    # Regex to match, gets the slots
-    re_match = f"{time_slot}.+?{where}.+?platserkvar:([0-9]|>[0-9])"
-
-    for i in all_time_slots:
-        # If slot is None, then it hasn't found the timeslot.
-        slot = re.search(re_match, re.sub(" |\n|\r", "", i.text))
-
-        if slot:
-            if slot.group(1) == "0":
-                return None
-
-            # Returns link and if timeslot is available
-            return site + i.find("div", class_="button-holder").find("a")["href"]
-    return None
-
-
 def post_data(url_str: str):
     try:
-        response = requests.get(url_str)
+        response = requests.get(url_str, timeout=10)
     except:
         print("Failed to get booking link")
         return False
@@ -175,6 +159,7 @@ def post_data(url_str: str):
         try:
             sent = requests.post(site + soup_response["action"], data=payload)
             if sent:
+                # Check if the post returned error. If no error, then the statement evaluates as None.
                 if not BeautifulSoup(sent.content, "html.parser").find("form").find("p", class_="error"):
                     print("Successfully Booked")
                     return True
